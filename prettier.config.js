f06ba2a904690fb647b9dc33f78617f4a5281c2d
Chris Lam <chris@lam.co.nz>
2019-03-10 20:49:17 +1300

    Add ESLint and Prettier configuration files

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: true,
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: false,
+    trailingComma: "none",
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: "avoid",
+    printWidth: 80
+};
